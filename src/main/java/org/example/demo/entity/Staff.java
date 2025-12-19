d9170dceb293b37f17ea373834a4e970e7a98bc6
AoIlI <209178683@qq.com>
2025-12-19 23:43:56 +0800

    2

---
+++ src/main/java/org/example/demo/entity/Staff.java
@@ -0,0 +1,22 @@
+package org.example.demo.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class Staff {
+    @Getter
+    @Setter
+    private String staffId;
+    @Getter
+    @Setter
+    private String name;
+    @Getter
+    @Setter
+    private String role;
+    @Getter
+    @Setter
+    private String phone;
+    @Getter
+    @Setter
+    private String specialty;
+}
